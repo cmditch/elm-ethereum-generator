fb145a424fe529e70311746f509da77df0ee0f85
Coury Ditch <cmditch@gmail.com>
2017-11-06 20:07:22 -0700

    Updated ABI and updated Types

--- JsonABI.hs
+++ JsonABI.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE OverloadedStrings    #-}
 {-# LANGUAGE TemplateHaskell      #-}
 
-module Network.Ethereum.Web3.JsonAbi (
+module JsonAbi (
     ContractABI(..)
   , Declaration(..)
   , FunctionArg(..)
   , EventArg(..)
-  , signature
-  , methodId
-  , eventId
+  -- , signature
+  -- , methodId
+  -- , eventId
   ) where
 
 import qualified Data.Text.Encoding as T
@@ -17,6 +17,7 @@ import Data.Monoid ((<>))
 import Data.Text (Text)
 import Data.Aeson.TH
 import Data.Aeson
+import Internal
 
 data FunctionArg = FunctionArg
   { funArgName :: Text
@@ -38,17 +39,27 @@ $(deriveJSON
     ''EventArg)
 
 data Declaration
-  = DConstructor { conInputs :: [FunctionArg] }
-  | DFunction { funName      :: Text
-              , funConstant  :: Bool
-              , funInputs    :: [FunctionArg]
-              , funOutputs   :: Maybe [FunctionArg]
-              }
-  | DEvent { eveName      :: Text
-           , eveInputs    :: [EventArg]
-           , eveAnonymous :: Bool
-           }
-  | DFallback { falPayable :: Bool }
+  = DConstructor  { conInputs :: [FunctionArg]
+                  , conPayable :: Bool
+                  , conStateMutability :: Text
+                  }
+
+  | DFunction     { funName      :: Text
+                  , funConstant  :: Bool
+                  , funInputs    :: [FunctionArg]
+                  , funOutputs   :: Maybe [FunctionArg]
+                  , conPayable :: Bool
+                  , conStateMutability :: Text
+                  }
+
+  | DEvent        { eveName      :: Text
+                  , eveInputs    :: [EventArg]
+                  , eveAnonymous :: Bool
+                  }
+
+  | DFallback     { falPayable :: Bool
+                  , conStateMutability :: Text
+                  }
   deriving (Show, Eq, Ord)
 
 $(deriveJSON (defaultOptions {
@@ -61,33 +72,33 @@ $(deriveJSON (defaultOptions {
 newtype ContractABI = ContractABI { unABI :: [Declaration] }
   deriving (Eq, Ord)
 
-instance Show ContractABI where
-    show (ContractABI c) = T.unpack $ T.unlines $
-        [ "Contract:" ]
-        ++ foldMap showConstructor c ++
-        [ "\tEvents:" ]
-        ++ foldMap showEvent c ++
-        [ "\tMethods:" ]
-        ++ foldMap showMethod c
-
-instance FromJSON ContractABI where
-    parseJSON = fmap ContractABI . parseJSON
-
-instance ToJSON ContractABI where
-    toJSON (ContractABI x) = toJSON x
-
-showConstructor :: Declaration -> [Text]
-showConstructor x = case x of
-    DConstructor{} -> ["\tConstructor " <> signature x]
-    _ -> []
-
-showEvent :: Declaration -> [Text]
-showEvent x = case x of
-    DEvent{} -> ["\t\t" <> signature x]
-    _ -> []
-
-showMethod :: Declaration -> [Text]
-showMethod x = case x of
-    DFunction{} ->
-        ["\t\t" <> methodId x <> " " <> signature x]
-    _ -> []
+-- instance Show ContractABI where
+--     show (ContractABI c) = T.unpack $ T.unlines $
+--         [ "Contract:" ]
+--         ++ foldMap showConstructor c ++
+--         [ "\tEvents:" ]
+--         ++ foldMap showEvent c ++
+--         [ "\tMethods:" ]
+--         ++ foldMap showMethod c
+--
+-- instance FromJSON ContractABI where
+--     parseJSON = fmap ContractABI . parseJSON
+--
+-- instance ToJSON ContractABI where
+--     toJSON (ContractABI x) = toJSON x
+--
+-- showConstructor :: Declaration -> [Text]
+-- showConstructor x = case x of
+--     DConstructor{} -> ["\tConstructor " <> signature x]
+--     _ -> []
+--
+-- showEvent :: Declaration -> [Text]
+-- showEvent x = case x of
+--     DEvent{} -> ["\t\t" <> signature x]
+--     _ -> []
+--
+-- showMethod :: Declaration -> [Text]
+-- showMethod x = case x of
+--     DFunction{} ->
+--         ["\t\t" <> methodId x <> " " <> signature x]
+--     _ -> []
